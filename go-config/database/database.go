e5980ff528a24a4b5156777acd0c487cd09f4e19
Guide <Chaiyaphat.in@hotmail.com>
2025-12-04 14:28:55 +0700

    test add panic to builddns

--- go-config/database/database.go
+++ go-config/database/database.go
@@ -30,6 +30,7 @@ type Database interface {
 }
 
 func BuildDns(options Options) (string, error) {
+	panic("🔥 NEW GO-CONFIG BUILD_DNS 🔥")
 	handleError := func(msg string) (string, error) { return "", errors.New(msg) }
 	if utils.IsBlank(options.UserName) {
 		return handleError("user name cannot be empty")
